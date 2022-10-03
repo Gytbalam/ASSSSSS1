f9d81924d8d6589e196fa7a9210c9fe4305dca40
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-03 13:27:30 -0400

    forth

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//installed 3rd party packages
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var app = express();
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-// view engine setup
+let app = express();
+
+// view Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
