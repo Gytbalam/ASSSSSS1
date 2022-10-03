7a0b91683d1f9fa73805538b1494cf73fefcbdc6
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-03 13:59:29 -0400

    fifth

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,25 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',
-   { title: 'Express',
+   { title: 'Home',
   mainT:'Main Title'
   });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index',{ title: 'About'});
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('index',{ title: 'Projects'});
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index',{ title: 'Services'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index',{ title: 'Contact'});
 });
 
 module.exports = router;
