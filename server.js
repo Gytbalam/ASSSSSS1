3dd085fc94c932a59d9decc1a72a911539e2e7f4
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-03 12:51:06 -0400

    third

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const app = express();
 const port = 4000;
 
 
-//display
+//Routes
 
-app.get('',(req,res) => {
-    res.send('Hello world')
+app.get('/',(req,res) => {
+    res.send('Hello world');
 })
+
+app.get('about',(req,res) => {
+    res.send('About me');
+})
+
 
 //after accepting the request
 
 app.listen(4000, ()=>{
-    console.log('App listening on port 4000')
+    console.log('App listening on port 4000');
     })
