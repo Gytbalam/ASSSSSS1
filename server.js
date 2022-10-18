642e23f5ced72863a00214ea908bfe582b2dc102
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-17 20:23:54 -0400

    strcutre

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ by Greg Lim was consoulted. */
  * Module dependencies.
  */
 
-var app = require('./app');
+var app = require('./config/app');
 var debug = require('debug')('assssss1:server');
 var http = require('http');
 
