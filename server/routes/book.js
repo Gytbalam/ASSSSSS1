1f159928ce08ed1b008bac279ba1399954eaa07f
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-20 02:04:41 -0400

    correciones

--- server/routes/book.js
+++ server/routes/book.js
@@ -5,9 +5,10 @@ let mongoose = require('mongoose');
 // connect to book model
 
 let Book = require('../models/book');
+let bookController = require('../controllers/book');
 
 // get route for the book list page
-router.get('/',(req,res,next) => {
+/* router.get('/',(req,res,next) => {
 Book.find((err, BookList) => {
     if(err)
     {
@@ -20,17 +21,21 @@ Book.find((err, BookList) => {
         res.render('book/list', {title: 'Books', BookList: BookList})
     }
     });
-});
+}); */
+
+router.get('/', bookController.displayBookList);
 
 
 // GET route for display the add list page - create operation 
 
-router.get('/add',(req,res,next) => {
+/* router.get('/add',(req,res,next) => {
     res.render('book/add', {title: 'Add Books'})
-});
+}); */
+
+router.get('/add', bookController.displayAddPage);
 
 // POST route for processing the add list page - create operation 
-router.post('/add',(req,res,next) => {
+/* router.post('/add',(req,res,next) => {
     let newBook = Book({
         "name": req.body.name,
         "author": req.body.author,
@@ -52,11 +57,13 @@ router.post('/add',(req,res,next) => {
         }
     });
 
-});
+}); */
+
+router.post('/add', bookController.processAddPage);
 
 // EDIT route for display the edit list page - create operation 
 
-router.get('/edit/:id',(req,res,next) => {
+/* router.get('/edit/:id',(req,res,next) => {
     let id = req.params.id;
 
     Book.findById(id,(err, bookToEdit) => {
@@ -72,10 +79,13 @@ router.get('/edit/:id',(req,res,next) => {
         }
     });
 });
+ */
+
+router.get('/edit/:id', bookController.displayEditPage);
 
 // POST route for display the edit list page - update operation 
 
-router.post('/edit/:id',(req,res,next) => {
+/* router.post('/edit/:id',(req,res,next) => {
     let id = req.params.id
 
     let updatedBook = Book({
@@ -101,10 +111,13 @@ Book.updateOne({ _id: id}, updatedBook, (err) => {
 });
 
 });
+ */
+
+router.post('/edit/:id', bookController.processEditPage);
 
 // GET route to perform a book deletion- DELETE op
 
-router.get('/delete/:id',(req,res,next) => {
+/* router.get('/delete/:id',(req,res,next) => {
     let id =  req.params.id;
 
     Book.remove({ _id: id}, (err) => {
@@ -119,7 +132,9 @@ router.get('/delete/:id',(req,res,next) => {
         res.redirect('/book-list');
         }
     });
-});
+}); */
+
+router.get('/delete/:id', bookController.performDelete);
 
 module.exports = router;
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,33 +7,48 @@ Assignment 1  */
 following the steps of educational videos. Also the book Beginning Node.js, Express & MongoDB Development (2019)
 by Greg Lim was consoulted. */
 
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+//index of controllers
+
+let indexController = require('../controllers/index');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/* BEFORE: router.get('/', function(req, res, next) {
   res.render('index',
    { title: 'Home' });
-});
-
-router.get('/about', function(req, res, next) {
-  res.render('about',{ title: 'About me'});
-});
-
-router.get('/projects', function(req, res, next) {
-  res.render('projects',{ title: 'Projects'});
-});
-
-router.get('/services', function(req, res, next) {
-  res.render('services',{ title: 'Services'});
-});
-
-router.get('/contact', function(req, res, next) {
-  res.render('contact',{ title: 'Contact'});
-});
-
-/* router.get('/book', function(req, res, next) {
-  res.render('book',{ title: 'Book List'});
 }); */
+
+router.get('/', indexController.displayHomePage);
+
+router.get('/home', indexController.displayHomePage);
+
+router.get('/about', indexController.displayAboutPage);
+
+/* BEFORE router.get('/about', function(req, res, next) {
+  res.render('about',{ title: 'About me'});
+}); */
+
+router.get('/projects', indexController.displayProjectsPage);
+
+/* router.get('/projects', function(req, res, next) {
+  res.render('projects',{ title: 'Projects'});
+}); */
+
+router.get('/services', indexController.displayServicesPage);
+
+/* router.get('/services', function(req, res, next) {
+  res.render('services',{ title: 'Services'});
+}); */
+
+router.get('/contact', indexController.displayContactPage);
+
+/* router.get('/contact', function(req, res, next) {
+  res.render('contact',{ title: 'Contact'});
+}); */
+
+
 
 module.exports = router;
