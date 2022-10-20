35f0eeededa70c23a54db7de36f5ae1175383f44
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-20 01:02:13 -0400

    Button change

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -2,11 +2,31 @@
 ID: 301184971
 DATE: 05.10.22
 Assignment 1  */
+
+//const { event } = require("jquery");
+
+//functions from client side view
+
 (function(){
+
     function Start()
     {
-        console.log("app started...");
+        console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=>{
+                if(!confirm("Are you sure?")) 
+                {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+            });
+        }
     }
 
-    window.addEventListener("load",Start);
+    window.addEventListener("load", Start);
+
 })();
