412be7380b24298d1d4e0a27c3bb0c77015807b1
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-25 15:13:49 -0400

    work with atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports =
 {
 
-    "URI" : "mongodb://127.0.0.1/book_store"
+    //"URI" : "mongodb://127.0.0.1/book_store"
+    "URI" : "mongodb+srv://Ymir:PHGIek5leP41Dkxf@webdevcluster.jtblrsq.mongodb.net/?retryWrites=true&w=majority"
     //"URI" : "mongodb://localhost/book_store"
 }
